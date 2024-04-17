4dfbb3dfe5b36327f6a1491c8f993784fe7ba8fa
Evan <suzukaze.hazuki2020@gmail.com>
2024-04-17 13:30:44 +0900

    useDevicesList

---
+++ pages/useDeviceOrientation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>是否支持：{{ isSupported }}</div>
+    <div>是否绝对：{{ isAbsolute }}</div>
+    <div>透明度：{{ alpha }}</div>
+    <div>beta: {{ beta }}</div>
+    <div>gamma: {{ gamma }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useDeviceOrientation } from "@vueuse/core";
+
+const { isSupported, isAbsolute, alpha, beta, gamma } = useDeviceOrientation();
+</script>
---
+++ pages/useDevicePixelRatio.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import { useDevicePixelRatio } from "@vueuse/core";
+
+const { pixelRatio } = reactive(useDevicePixelRatio());
+</script>
+
+<template>
+  <div>Device Pixel Ratio:</div>
+  <pre>{{ pixelRatio }}</pre>
+</template>
