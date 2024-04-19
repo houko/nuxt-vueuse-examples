012362b64b3b7fde2bef04268aacb37feb79001e
Evan <suzukaze.hazuki2020@gmail.com>
2024-04-19 19:47:30 +0900

    auto-animation

---
+++ pages/animation/dropdown.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import autoAnimate from "@formkit/auto-animate";
+
+const dropdown = ref(); // we need a DOM node
+const show = ref(false);
+
+onMounted(() => {
+  autoAnimate(dropdown.value); // thats it!
+});
+</script>
+
+<template>
+  <div ref="dropdown" class="dropdown">
+    <el-button class="dropdown-label" @click="show = !show">
+      Click me to open!
+    </el-button>
+    <p class="dropdown-content" v-if="show">这是dropdown的内容...</p>
+  </div>
+</template>
