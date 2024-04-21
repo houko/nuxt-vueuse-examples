3c369123eac42b2a70ee8cabe700a350ddc5cad5
Evan <suzukaze.hazuki2020@gmail.com>
2024-04-21 16:29:43 +0900

    util

---
+++ pages/basic/util.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>{{ formatNumber(1234) }}</div>
+  <div>{{ random([1, 2, 3, 4, 5]) }}</div>
+</template>
+
+<script setup lang="ts">
+import random from "~/utils/random";
+import { formatNumber } from "~/utils/format";
+</script>
